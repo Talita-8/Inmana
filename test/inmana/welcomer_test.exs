083a002c7dddad43c5e7d375e59ff9f43d834138
Talita-8 <tlthmf1@outlook.com>
2021-04-29 16:15:35 -0300

    Testes de Changeset

--- test/inmana/welcomer_test.exs
+++ test/inmana/welcomer_test.exs
@@ -6,10 +6,25 @@ defmodule Inmana.WelcomerTest do
   describe "welcome/1" do
     test "when the user is special, returns a special message" do
       params = %{"name" => "banana", "age" => "42"}
+      expect_result = {:ok, "You are very special, banana"}
 
       result = Welcomer.welcome(params)
+      assert result == expect_result
+    end
 
-      expect_result = {:ok, "You are very special, banana"}
+    test "when the user is not special, returns a message" do
+      params = %{"name" => "rafael", "age" => "25"}
+      expect_result = {:ok, "Welcome rafael"}
+
+      result = Welcomer.welcome(params)
+      assert result == expect_result
+    end
+
+    test "when the user is under age, returns an error" do
+      params = %{"name" => "rafael", "age" => "17"}
+      expect_result = {:error, "You shall no pass, rafael"}
+
+      result = Welcomer.welcome(params)
       assert result == expect_result
     end
   end
